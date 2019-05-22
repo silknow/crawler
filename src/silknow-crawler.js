143ad069cde16626317b160ade40f96d15a59b10
Thibault Ehrhart <thibault.ehrhart@gmail.com>
2019-05-22 15:49:13 +0200

    Add support for multi crawling
    - Add package "yargs" for parsing arguments

--- src/silknow-crawler.js
+++ src/silknow-crawler.js
@@ -1,29 +1,40 @@
+const { argv } = require('yargs');
+
 const crawlers = require('./crawlers');
 
 process.on('unhandledRejection', err => {
   throw err;
 });
 
-const Crawler = Object.values(crawlers)
-  .filter(c => process.argv[2] === c.id)
-  .pop();
+const targetCrawlers = [];
 
-if (!Crawler) {
+argv._.forEach(crawlerName => {
+  const crawler = Object.values(crawlers)
+    .filter(c => c.id === crawlerName)
+    .pop();
+  if (!crawler) {
+    console.error(`Crawler not found: ${crawlerName}`);
+  } else {
+    targetCrawlers.push(crawler);
+  }
+});
+
+if (!targetCrawlers.length) {
   console.log(
     `No crawler selected. Crawlers available: ${Object.values(crawlers)
       .map(c => c.id)
       .join(', ')}`
   );
 } else {
-  const crawler = new Crawler();
-
-  console.log(`Running crawler "${crawler.constructor.id}"`);
-  crawler
-    .start()
-    .then(() => {
-      console.log('DONE!');
-    })
-    .catch(err => {
-      console.error('Error:', err);
-    });
+  (async () => {
+    for (const Crawler of targetCrawlers) {
+      try {
+        const crawler = new Crawler(argv);
+        console.log(`Running crawler "${crawler.constructor.id}"`);
+        await crawler.start();
+      } catch (err) {
+        console.error('Error:', err);
+      }
+    }
+  })();
 }
