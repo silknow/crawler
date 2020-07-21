01cca0aeef8e0f7c7715870034e5fe672e85c0d8
Thibault Ehrhart <thibault.ehrhart@gmail.com>
2020-07-21 11:53:26 +0200

    check-silknow-urls: check for both missing urls and files

--- scripts/check-silknow-urls.py
+++ scripts/check-silknow-urls.py
@@ -1,28 +1,47 @@
 import argparse
 import csv
-from os import path
+import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--input', help="Input path of the missing urls CSV file")
-parser.add_argument('-o', '--output', help="Output path where the new CSV file will be stored")
+parser.add_argument('-o', '--output', help="Output directory where the new CSV files will be stored")
 parser.add_argument('-q', '--quiet', action='store_true', help="Do not print the list of missing files")
 args = parser.parse_args()
 
 with open(args.input) as csv_file:
   csv_reader = csv.reader(csv_file, delimiter=',')
 
-  with open(args.output, mode='w') as missing_file:
-    missing_writer = csv.writer(missing_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-    header = next(csv_reader)
-    missing_writer.writerow(header);
+  missing_urls_output = os.path.join(args.output, 'silknow-missing-urls.csv')
+  missing_files_output = os.path.join(args.output, 'silknow-missing-files.csv')
 
-    for row in csv_reader:
-      museum = row[3].split('/')[5]
-      filename = path.basename(row[3])
-      filepath = path.join(museum, filename)
+  with open(missing_urls_output, mode='w') as missing_url:
+    missing_url_writer = csv.writer(missing_url, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
-      if not path.exists(filepath):
-        missing_writer.writerow(row)
+    with open(missing_files_output, mode='w') as missing_file:
+      missing_file_writer = csv.writer(missing_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+      header = next(csv_reader)
+      missing_file_writer.writerow(header);
 
-        if not args.quiet:
-          print(filepath + ' does not exist')
+      filepath_cache = []
+
+      for row in csv_reader:
+        museum = row[3].split('/')[5]
+        filename = os.path.basename(row[3])
+        filepath = os.path.normpath(os.path.join(museum, filename))
+        filepath_cache.append(filepath)
+
+        if not os.path.exists(filepath):
+          missing_file_writer.writerow(row)
+
+          if not args.quiet:
+            print(filepath + ' does not exist in files')
+
+      for root, dirs, files in os.walk('./'):
+        for file in files:
+          if file.endswith('.jpg'):
+            filepath = os.path.normpath(os.path.join(root, file))
+            if filepath not in filepath_cache:
+              missing_url_writer.writerow([filepath])
+
+              if not args.quiet:
+                print(filepath + ' does not exist in query result')
--- scripts/list_missing_images.sh
+++ scripts/list_missing_images.sh
@@ -6,8 +6,7 @@ source "${SCRIPTPATH}/config.sh"
 source "${SCRIPTPATH}/lib.sh"
 
 py_path="${SCRIPTPATH}/check-silknow-urls.py"
-out_filename="silknow-urls-missing.csv"
-out_local_path="${SCRIPTPATH}/${out_filename}"
+out_local_path="${SCRIPTPATH}"
 
 if [[ ! "${py_path}" ]]; then
   echo "Python script check-silknow-urls.py not found"
@@ -29,11 +28,11 @@ csv_path="${SCRIPTPATH}/${csv_filename}"
 csv_destpath="${media_folder}/${csv_filename}"
 py_filename="$(basename -- "${py_path}")"
 py_destpath="${media_folder}/${py_filename}"
-out_remote_path="${media_folder}/${out_filename}"
+out_remote_path="${media_folder}"
 query_filename="imageURLs.sparql"
 query_path="${SCRIPTPATH}/${query_filename}"
 
-curl -o "${csv_path}" -H "Accept: text/csv" --data-urlencode "query@${query_path}" "${sparql_endpoint}" || exit 1
+curl -L -o "${csv_path}" -H "Accept: text/csv" --data-urlencode "query@${query_path}" "${sparql_endpoint}" || exit 1
 
 if [[ ! -f "${csv_path}" ]]; then
   echo "${csv_path}: Not found"
@@ -47,14 +46,17 @@ scp "${csv_path}" "${py_path}" "${media_user}"@silknow.uv.es:"${media_folder}" |
 dotsleep 30
 
 # # Execute python script on remote server to check for dead urls
-cmd="cd '${media_folder}'; python check-silknow-urls.py -q -i '${csv_destpath}' -o '${out_filename}'"
+cmd="cd '${media_folder}'; python check-silknow-urls.py -q -i '${csv_destpath}' -o '${out_remote_path}'"
 ssh "${media_user}"@silknow.uv.es "${cmd}" || exit 1
 
 # Prevent timeout from media server
 dotsleep 30
 
 # Download results from media server
-scp "${media_user}"@silknow.uv.es:"${out_remote_path}" "${out_local_path}" || exit 1
+scp "${media_user}"@silknow.uv.es:"${out_remote_path}/silknow-missing-urls.csv" "${out_local_path}/silknow-missing-urls.csv" || exit 1
+scp "${media_user}"@silknow.uv.es:"${out_remote_path}/silknow-missing-files.csv" "${out_local_path}/silknow-missing-files.csv" || exit 1
 
-count=$(($(wc -l "${out_filename}" | awk '{ print $1 }')-1))
-echo "Found ${count} missing images (see file ${out_filename})"
+count_urls=$(($(wc -l "${out_local_path}/silknow-missing-urls.csv" | awk '{ print $1 }')-1))
+count_files=$(($(wc -l "${out_local_path}/silknow-missing-files.csv" | awk '{ print $1 }')-1))
+echo "Found ${count_urls} missing images urls (see file ${out_local_path}/silknow-missing-urls.csv})"
+echo "Found ${count_files} missing images urls (see file ${out_local_path}/silknow-missing-files.csv})"
